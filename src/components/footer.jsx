f1af4089e159173856c9cd18316d3999621450f2
Ariel dos Santos Nunes <arielsn@outlook.com>
2022-04-24 08:38:49 -0300

    Added footer to project

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+	return (
+		<footer>
+			<p className="my-8 border-t-2 py-6 text-center">
+				Você pode encontrar o repositório desse website em <a href="https://github.com/ArielSNunes/arielsn.dev" className="text-blue-600 font-bold hover:text-purple-600" target='_blank' rel="noopener">ArielSNunes/arielsn.dev</a>
+			</p>
+		</footer>
+	)
+}
+
+export default Footer
