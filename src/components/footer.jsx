2e099f131adae3bc0582d93afb67886d4d8404c8
Ariel dos Santos Nunes <arielsn@outlook.com>
2022-04-24 08:55:53 -0300

    Updated link attr

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
 	return (
 		<footer>
 			<p className="my-8 border-t-2 py-6 text-center">
-				Você pode encontrar o repositório desse website em <a href="https://github.com/ArielSNunes/arielsn.dev" className="text-blue-600 font-bold hover:text-purple-600" target='_blank' rel="noopener">ArielSNunes/arielsn.dev</a>
+				Você pode encontrar o repositório desse website em <a href="https://github.com/ArielSNunes/arielsn.dev" className="text-blue-600 font-bold hover:text-purple-600" target='_blank' rel="noreferrer">ArielSNunes/arielsn.dev</a>
 			</p>
 		</footer>
 	)
