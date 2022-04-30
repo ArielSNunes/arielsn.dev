c084d5434cf38410484fed5bed3a1be4e7d1b5fb
Ariel dos Santos Nunes <arielsn@outlook.com>
2022-04-30 17:06:06 -0300

    Included nProgress lib to project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,12 @@
+import NProgress from 'nprogress'
+import Router from 'next/router'
+import 'nprogress/nprogress.css'
+
 import '../styles/globals.css'
+
+Router.events.on("routeChangeStart", NProgress.start)
+Router.events.on("routeChangeComplete", NProgress.done)
+Router.events.on("routeChangeError", NProgress.done)
 
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />
