fbb86b5ac4a93ee90f64b995b221521fdbf56417
Ariel dos Santos Nunes <arielsn@outlook.com>
2022-05-14 10:49:21 -0300

    Updated layout

--- src/components/single-skill.jsx
+++ src/components/single-skill.jsx
@@ -6,7 +6,7 @@ const SingleSkill = ({ skill }) => {
 		<div className="relative pt-1">
 			<div className="flex mb-2 items-center justify-between">
 				<div className='flex justify-center items-center align-center'>
-					<span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-orange border-orange border-2">
+					<span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-md text-orange border-orange border-2">
 						{skill.tech}
 					</span>
 					{skill.studying && <GiWhiteBook title='Em estudo' className='text-orange ml-2' />}
@@ -17,7 +17,7 @@ const SingleSkill = ({ skill }) => {
 					</span>
 				</div>
 			</div>
-			<div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-pink-200 border-orange border-2">
+			<div className="overflow-hidden h-2 mb-4 text-xs flex rounded-sm bg-pink-200 border-orange border-2">
 				<div style={{ width: `${percentage}%` }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-orange"></div>
 			</div>
 		</div>
